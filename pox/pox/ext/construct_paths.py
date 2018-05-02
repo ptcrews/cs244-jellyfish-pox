c5c064b076be3699b8f5984421edbd41e3a824fb
Paul Crews <ptcrews@stanford.edu>
2018-05-02 01:39:06 -0700

    Looks like K shortest paths works now

--- pox/pox/ext/construct_paths.py
+++ pox/pox/ext/construct_paths.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import copy
 
 class Paths ():
     # Adjacency map.  [sw1][sw2] -> port from sw1 to sw2
@@ -19,7 +20,7 @@ class Paths ():
     def dijkstra (self, switches, adjacency, source, dest):
         # List of all the switches by dpid
         # TODO: Should probably be a set...
-        Q = switches
+        Q = copy.deepcopy(switches)
 
         dist = {}
         prev = {}
@@ -36,58 +37,96 @@ class Paths ():
                 if dist[u] < min_found:
                     min_found = u
 
+            # No path found
+            if min_found == float('Inf'):
+                return []
+
             Q.remove(min_found)
             if min_found == dest:
                 break
 
             for v,port in adjacency[min_found].iteritems():
-                if dist[min_found] + 1 < dist[v]:
+                if dist[min_found] + 1 < dist[v] and port != None:
                     dist[v] = dist[min_found] + 1
                     prev[v] = min_found
 
         path = []
-        u = dest
-        while prev[u] != None:
-            # Push the next hop id and port
-            # Note that this link *must* exist in the adjacency graph
-            # TODO: Deeply consider whether to have the (node, port_to_get_to_node)
-            # or (node, egress_port) as the tuple
-            path.insert(0, (u, adjacency[prev[u]][u]))
-            u = prev[u]
-        # Finally push ourselves and no port?
-        path.insert(0, (u, None))
+        prev_node = dest
+        path.insert(0, (prev_node, None))
+        while prev_node in prev and prev[prev_node] != None:
+            cur_node = prev[prev_node]
+            path.insert(0, (cur_node, adjacency[cur_node][prev_node]))
+            prev_node = cur_node
+
+        if prev_node != source:
+            return []
 
         return path
 
-    # TODO: Many, numerous, incalcuable problems with all of this :/
-    def yen_ksp(switches, adjacency, source, dest, K):
+    def delete_node(self, adjacency, switches, node):
+        for i in range(len(switches)):
+            self.remove_edges(adjacency, node, i)
+
+    def dump(self, adjacency):
+        for node in adjacency:
+            print "Node: " + str(node)
+            for connected in adjacency[node]:
+                print "\t Connected to " + str(connected) + " with value " + \
+                    str(adjacency[node][connected])
+
+    #def check_integrity
+
+    def remove_edges(self, adjacency, first_node, second_node):
+        if adjacency[first_node][second_node] != None:
+            del adjacency[first_node][second_node]
+        if adjacency[second_node][first_node] != None:
+            del adjacency[second_node][first_node]
+
+    def find_remove_min(self, path_list):
+        if len(path_list) == 0:
+            return None
+
+        min_index = 0
+        for i in range(len(path_list)):
+            if len(path_list[i]) < len(path_list[min_index]):
+                min_index = i
+        res = path_list[i]
+        del path_list[i]
+        return res
+
+    # This implementation now *kind of* works
+    # Heavily inspired by the Wikipedia psuedo-code for Yen's K shortest paths
+    # algorithm
+    def yen_ksp(self, switches, adjacency, source, dest, K):
         A = []
         # Find the shortest path
-        A.append(dijkstra(switches, adjacency, source, dest))
+        A.append(self.dijkstra(switches, adjacency, source, dest))
 
-        B = {}
+        B = []
 
-        # TODO: Off by one?
-        for k in range(1..K):
-            for i in range(len(A[k-1])-1):
-                spurNode = A[k-1][i][0] # Should retrieve i-th node
+        for k in range(1, K+1):
+            for i in range(0, len(A[k-1])-1):
+                adjacency_copy = copy.deepcopy(adjacency)
+                spurNode = A[k-1][i] # Should retrieve i-th node
 
-                rootPath = A[k-1][0..i]
+                rootPath = A[k-1][:i+1]
 
                 for p in A:
-                    if rootPath == p[0..i]:
-                        # TODO: Confirm removal here is correct
-                        adjacency[p[i]][p[i+1]] = None
+                    if rootPath == p[:i+1]:
+                        first_node = p[i][0]
+                        second_node = p[i+1][0]
+                        self.remove_edges(adjacency_copy, first_node, second_node)
 
                 for node in rootPath:
-                    if node != spurNode:
+                    if node == spurNode:
                         continue
-                    # TODO: how to remove node from graph?? just invalidate edges?
-                    # remove node from Graph
+                    self.delete_node(adjacency_copy, switches, node[0])
 
-                spurPath = dijkstra(switches, adjacency, spurNode, dest)
+                spurPath = self.dijkstra(switches, adjacency_copy, spurNode[0], dest)
+                if spurPath == []:
+                    continue
 
-                totalPath = rootPath + spurPath
+                totalPath = rootPath + spurPath[1:]
                 B.append(totalPath)
 
                 # Restore edges to graph... :/
@@ -95,11 +134,17 @@ class Paths ():
             if len(B) == 0:
                 break
 
-            B.sort()
-            A[k] = B[0]
-            B.pop()
+            A.append(self.find_remove_min(B))
 
         return A
+
+    def all_k_shortest_paths (self, switches, adjacency, K):
+        path_map = defaultdict(lambda:defaultdict(lambda:(None,None)))
+        for i in switches:
+            for j in switches:
+                res = self.yen_ksp(switches, adjacency, i, j, K)
+                path_map[i][j] = res
+                print "Paths from " + str(i) + " to " + str(j) + " : " + str(res)
 
     def simple_test (self):
         switches = [0, 1, 2, 3, 4]
@@ -107,14 +152,35 @@ class Paths ():
         adjacency = defaultdict(lambda:defaultdict(lambda:(None,None)))
         # Simple tree topology
         adjacency[0][1] = 0
-        adjacency[0][2] = 1
         adjacency[1][0] = 0
+
+        adjacency[0][2] = 1
+        adjacency[2][0] = 1
+
+        adjacency[1][0] = 0
+        adjacency[0][1] = 0
+
         adjacency[1][3] = 1
+        adjacency[3][1] = 1
+
         adjacency[2][0] = 0
+        adjacency[0][2] = 0
+
         adjacency[2][4] = 1
+        adjacency[4][2] = 1
+
         adjacency[3][1] = 0
+        adjacency[1][3] = 0
+
         adjacency[4][2] = 0
-        print str(self.dijkstra(switches, adjacency, 3, 4))
+        adjacency[2][4] = 0
+
+        adjacency[3][4] = 1
+        adjacency[4][3] = 1
+
+        res = self.yen_ksp(switches, adjacency, 3, 4, 2)
+        print "K shortest paths: " + str(res)
+        self.all_k_shortest_paths(switches, adjacency, 2)
 
 if __name__ == "__main__":
     paths = Paths()
