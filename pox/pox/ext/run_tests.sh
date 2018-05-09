f36564e6effd83c5673f55b8a34406161de857a6
Paul Crews <ptcrews@stanford.edu>
2018-05-09 13:55:37 -0700

    Added defaults to run_tests, build_topology.

--- pox/pox/ext/run_tests.sh
+++ pox/pox/ext/run_tests.sh
@@ -11,13 +11,17 @@ mkdir test_ECMP_1
 mkdir test_ECMP_8
 
 # Set up test-specific variables
+# These are the defaults for the test that gave
+# the most Jellyfish-like result
 export N_PORTS=15
 export INTER_PORTS=5
 export NUM_SW=30
 export SWITCH_BW=100
 export HOST_BW=10
 
-for ((i=0;i<5;i++))
+# We only run two iterations here, as otherwise the test suite becomes unstable.
+# Feel free to change this
+for ((i=0;i<2;i++))
 do
   sudo mn -c && sudo -E TEST_NAME=KSP N_FLOWS=1 TEST_NUM=$i python2 ./build_topology.py
   sudo killall -9 dhclient
